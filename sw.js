7e908e4cf368845c1a7c9649daa1d76fb773f8d6
alexzavalny <alex@zavalny.com>
2024-06-16 16:48:43 +0300

    debug

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "v2"; // Change this version whenever you update your PWA
+const CACHE_NAME = "v3"; // Change this version whenever you update your PWA
 const urlsToCache = [
   "index.html",
   "styles.css",
